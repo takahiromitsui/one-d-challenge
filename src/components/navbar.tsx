5536b12602287e472cf8adc0012359392bb6a183
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2025-04-09 20:37:43 +0200

    feat: max-width-wrapper and navbarを作成

---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import MaxWidthWrapper from './max-width-wrapper';
+
+export default function Navbar() {
+	return (
+		<div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+			<header className='relative bg-white'>
+				<MaxWidthWrapper>
+					<Link href='/'>
+						<div className='flex items-center justify-between h-16  border-b border-gray-200'>
+							<h1 className='text-xl font-bold text-gray-800 py-8 cursor-pointer hover:text-gray-600 transition-colors duration-200'>
+								Github Repository Search
+							</h1>
+						</div>
+					</Link>
+				</MaxWidthWrapper>
+			</header>
+		</div>
+	);
+}
